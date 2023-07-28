4c5b0495527d39fdfccc2c482968301094da276c
DavHau <d.hauer.it@gmail.com>
2023-07-28 14:57:23 +0200

    devShells.default: init

---
+++ flake-parts/devShells/default.nix
@@ -0,0 +1,15 @@
+{
+  perSystem = {
+    lib,
+    pkgs,
+    self',
+    ...
+  }: {
+    devShells.default = pkgs.mkShell {
+      packages = [
+        pkgs.tea
+        self'.packages.tea-create-pr
+      ];
+    };
+  };
+}
