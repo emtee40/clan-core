00dac64884c1405ec7e8e9e5439f76ddb0133e8b
lassulus <git@lassul.us>
2023-08-04 17:05:30 +0200

    clanModules zerotier: add controller module

--- clanModules/zerotier.nix
+++ clanModules/zerotier.nix
@@ -1,4 +1,7 @@
 { config, lib, ... }:
+let
+  cfg = config.clan.networking.zerotier;
+in
 {
   options.clan.networking.zerotier = {
     networkId = lib.mkOption {
@@ -6,6 +9,16 @@
       description = ''
         zerotier networking id
       '';
+    };
+    controller = {
+      enable = lib.mkEnableOption "turn this machine into the networkcontroller";
+      public = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          everyone can join a public network without having the administrator to accept
+        '';
+      };
     };
   };
   config = {
@@ -23,7 +36,52 @@
     networking.firewall.interfaces."zt+".allowedUDPPorts = [ 5353 ];
     services.zerotierone = {
       enable = true;
-      joinNetworks = [ config.clan.networking.zerotier.networkId ];
+      joinNetworks = [ cfg.networkId ];
     };
+  } // lib.mkIf cfg.controller.enable {
+    systemd.tmpfiles.rules = [
+      "L+ /var/lib/zerotierone/controller.d/network/${cfg.networkId}.json - - - - ${pkgs.writeText "net.json" builtins.toJSON {
+        authTokens = [
+          null
+        ];
+        authorizationEndpoint = "";
+        capabilities = [];
+        clientId = "";
+        creationTime = 1688396140187;
+        dns = [];
+        enableBroadcast = true;
+        id = cfg.networkId;
+        ipAssignmentPools = [];
+        mtu = 2800;
+        multicastLimit = 32;
+        name = "";
+        uwid = cfg.networkId;
+        objtype = "network";
+        private = true;
+        remoteTraceLevel = 0;
+        remoteTraceTarget = null;
+        revision = 1;
+        routes = [];
+        rules = [
+          {
+            not = false;
+            or = false;
+            type = "ACTION_ACCEPT";
+          }
+        ];
+        rulesSource = "";
+        ssoEnabled = false;
+        tags = [];
+        v4AssignMode = {
+          zt = false;
+        };
+        v6AssignMode = {
+          "6plane" = false;
+          rfc4193 = false;
+          zt = false;
+        };
+      }}"
+    ];
   };
 }
+
