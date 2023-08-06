3c7511e5b8d91ac64cc4b2fc7bba983da39dd34a
Luis-Hebendanz <luis.nixos@gmail.com>
2023-08-06 17:30:26 +0200

    Added Edit and single item select

--- pkgs/ui/src/app/nodes/NodeList.tsx
+++ pkgs/ui/src/app/nodes/NodeList.tsx
@@ -23,45 +23,50 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 import CircleIcon from '@mui/icons-material/Circle';
+import Stack from '@mui/material/Stack/Stack';
+import ModeIcon from '@mui/icons-material/Mode';
+import ClearIcon from '@mui/icons-material/Clear';
 
 interface Data {
-  status: boolean;
-  carbs: number;
-  fat: number;
   name: string;
-  protein: number;
+  id: string;
+  status: boolean;
+  last_seen: number;
 }
 
 function createData(
   name: string,
+  id: string,
   status: boolean,
-  fat: number,
-  carbs: number,
-  protein: number,
+  last_seen: number,
+
 ): Data {
+  if (status && last_seen > 0) {
+    console.error("Last seen should be 0 if status is true");
+  }
+
   return {
     name,
+    id,
     status,
-    fat,
-    carbs,
-    protein,
+    last_seen: last_seen,
   };
 }
 
 const rows = [
-  createData('Cupcake', true, 3.7, 67, 4.3),
-  createData('Donut', true, 25.0, 51, 4.9),
-  createData('Eclair', false, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', false, 6.0, 24, 4.0),
-  createData('Gingerbread', false, 16.0, 49, 3.9),
-  createData('Honeycomb', true, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', true, 9.0, 37, 4.3),
-  createData('Jelly Bean', true, 0.0, 94, 0.0),
-  createData('KitKat', false, 26.0, 65, 7.0),
-  createData('Lollipop', false, 0.2, 98, 0.0),
-  createData('Marshmallow', false, 0, 81, 2.0),
-  createData('Nougat', true, 19.0, 9, 37.0),
-  createData('Oreo', true, 18.0, 63, 4.0),
+  createData('Matchbox', "42:0:f21:6916:e333:c47e:4b5c:e74c", true, 0),
+  createData('Ahorn', "42:0:3c46:b51c:b34d:b7e1:3b02:8d24", true, 0),
+  createData('Yellow', "42:0:3c46:98ac:9c80:4f25:50e3:1d8f", false, 16.0),
+  createData('Rauter', "42:0:61ea:b777:61ea:803:f885:3523", false, 6.0),
+  createData('Porree', "42:0:e644:4499:d034:895e:34c8:6f9a", false, 13),
+  createData('Helsinki', "42:0:3c46:fd4a:acf9:e971:6036:8047", true, 0),
+  createData('Kelle', "42:0:3c46:362d:a9aa:4996:c78e:839a", true, 0),
+  createData('Shodan', "42:0:3c46:6745:adf4:a844:26c4:bf91", true, 0.0),
+  createData('Qubasa', "42:0:3c46:123e:bbea:3529:db39:6764", false, 7.0),
+  createData('Green', "42:0:a46e:5af:632c:d2fe:a71d:cde0", false, 2),
+  createData('Gum', "42:0:e644:238d:3e46:c884:6ec5:16c", false, 0),
+  createData('Xu', "42:0:ca48:c2c2:19fb:a0e9:95b9:794f", true, 0),
+  createData('Zaatar', "42:0:3c46:156e:10b6:3bd6:6e82:b2cd", true, 0),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -80,7 +85,7 @@ function getComparator<Key extends keyof any>(
   order: Order,
   orderBy: Key,
 ): (
-  a: { [key in Key]: number | string | boolean},
+  a: { [key in Key]: number | string | boolean },
   b: { [key in Key]: number | string | boolean },
 ) => number {
   return order === 'desc'
@@ -108,53 +113,39 @@ interface HeadCell {
   disablePadding: boolean;
   id: keyof Data;
   label: string;
-  numeric: boolean;
+  alignRight: boolean;
 }
 
 const headCells: readonly HeadCell[] = [
   {
     id: 'name',
-    numeric: false,
-    disablePadding: true,
+    alignRight: false,
+    disablePadding: false,
     label: 'Display Name & ID',
   },
   {
     id: 'status',
-    numeric: false,
+    alignRight: false,
     disablePadding: false,
     label: 'Status',
   },
   {
-    id: 'fat',
-    numeric: true,
+    id: 'last_seen',
+    alignRight: false,
     disablePadding: false,
-    label: 'Fat (g)',
-  },
-  {
-    id: 'carbs',
-    numeric: true,
-    disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Last Seen',
   },
 ];
 
 interface EnhancedTableProps {
-  numSelected: number;
   onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
   order: Order;
   orderBy: string;
   rowCount: number;
 }
 
 function EnhancedTableHead(props: EnhancedTableProps) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+  const { order, orderBy, onRequestSort } =
     props;
   const createSortHandler =
     (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
@@ -164,21 +155,10 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
+            align={headCell.alignRight ? 'right' : 'left'}
             padding={headCell.disablePadding ? 'none' : 'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
           >
@@ -202,70 +182,118 @@ function EnhancedTableHead(props: EnhancedTableProps) {
 }
 
 interface EnhancedTableToolbarProps {
-  numSelected: number;
+  selected: string | undefined;
+  onClear: () => void;
+}
+
+function renderLastSeen(last_seen: number) {
+  return (
+    <Typography component="div" align="left" variant="body1">
+      {last_seen} days ago
+    </Typography>
+  );
+}
+
+function renderName(name: string, id: string) {
+  return (
+    <Stack>
+      <Typography component="div" align="left" variant="body1">
+        {name}
+      </Typography>
+      <Typography color="grey" component="div" align="left" variant="body2">
+        {id}
+      </Typography>
+    </Stack>
+  );
 }
 
 function renderStatus(status: boolean) {
   if (status) {
-    return <Typography variant="body1"><CircleIcon fontSize="small" color="success" style={{ fontSize: 16, verticalAlign: 'middle' }} /> Online </Typography> ;
+    return (
+      <Stack direction="row" alignItems="center" gap={1}>
+        <CircleIcon color="success" style={{ fontSize: 15 }} />
+        <Typography component="div" align="left" variant="body1">
+          Online
+        </Typography>
+      </Stack>
+    );
   }
-  return <CircleIcon fontSize="small" color="error" />;
+  return (
+    <Stack direction="row" alignItems="center" gap={1}>
+      <CircleIcon color="error" style={{ fontSize: 15 }} />
+      <Typography component="div" align="left" variant="body1">
+        Offline
+      </Typography>
+    </Stack>
+  );
 }
 
 function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-  const { numSelected } = props;
+  const { selected, onClear } = props;
+  const somethingSelected = selected !== undefined;
 
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
+  const handleSomethingSelected = () => {
+    if (somethingSelected) {
+      return (
+        <Toolbar
+          sx={{
+            pl: { sm: 2 },
+            pr: { xs: 1, sm: 1 },
+            bgcolor: (theme) =>
             alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Nodes
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
+          }}>
+          <Tooltip title="Clear">
+            <IconButton onClick={onClear}>
+              <ClearIcon />
+            </IconButton>
+          </Tooltip>
+          <Typography
+            sx={{ flex: '1 1 100%' }}
+            color="inherit"
+            style={{fontSize: 18, marginBottom: 2.5, marginLeft: 3}}
+            component="div"
+          >
+            {selected} selected
+          </Typography>
+          <Tooltip title="Edit">
+            <IconButton>
+              <ModeIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      );
+    } else {
+      return (
+        <Toolbar
+          sx={{
+            pl: { sm: 2 },
+            pr: { xs: 1, sm: 1 }
+          }}>
+          <Typography
+            sx={{ flex: '1 1 100%' }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Nodes
+          </Typography>
+          <Tooltip title="Filter list">
+            <IconButton>
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar >
+      );
+    }
+  };
+
+  return handleSomethingSelected();
 }
 
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState<Order>('asc');
   const [orderBy, setOrderBy] = React.useState<keyof Data>('status');
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [selected, setSelected] = React.useState<string | undefined>(undefined);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -279,33 +307,12 @@ export default function EnhancedTable() {
     setOrderBy(property);
   };
 
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
   const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
+    if (selected === name) {
+      setSelected(undefined);
+    } else {
+      setSelected(name);
     }
-
-    setSelected(newSelected);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -321,7 +328,7 @@ export default function EnhancedTable() {
     setDense(event.target.checked);
   };
 
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+  const isSelected = (name: string) => name === selected;
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
@@ -339,7 +346,7 @@ export default function EnhancedTable() {
   return (
     <Box sx={{ width: '100%' }}>
       <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
+        <EnhancedTableToolbar selected={selected} onClear={() => setSelected(undefined)}/>
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
@@ -347,10 +354,8 @@ export default function EnhancedTable() {
             size={dense ? 'small' : 'medium'}
           >
             <EnhancedTableHead
-              numSelected={selected.length}
               order={order}
               orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
@@ -370,7 +375,7 @@ export default function EnhancedTable() {
                     selected={isItemSelected}
                     sx={{ cursor: 'pointer' }}
                   >
-                    <TableCell padding="checkbox">
+                    {/* <TableCell padding="checkbox">
                       <Checkbox
                         color="primary"
                         checked={isItemSelected}
@@ -378,19 +383,16 @@ export default function EnhancedTable() {
                           'aria-labelledby': labelId,
                         }}
                       />
-                    </TableCell>
+                    </TableCell> */}
                     <TableCell
                       component="th"
                       id={labelId}
                       scope="row"
-                      padding="none"
                     >
-                      {row.name}
+                      {renderName(row.name, row.id)}
                     </TableCell>
                     <TableCell align="right">{renderStatus(row.status)}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="right">{renderLastSeen(row.last_seen)}</TableCell>
                   </TableRow>
                 );
               })}
@@ -406,6 +408,7 @@ export default function EnhancedTable() {
             </TableBody>
           </Table>
         </TableContainer>
+        {/* TODO: This creates the error Warning: Prop `id` did not match. Server: ":RspmmcqH1:" Client: ":R3j6qpj9H1:" */}
         <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
